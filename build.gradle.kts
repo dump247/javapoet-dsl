4d5a95c0e0bde4dfe53b714c56ea97e38cb0c5b5
Cory Thomas <dump247@users.noreply.github.com>
2020-02-17 07:47:15 -0600

    Publish sources jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,10 +35,16 @@ tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
 
+val sourcesJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("sources")
+    from(sourceSets.main.get().allSource)
+}
+
 publishing {
     publications {
         create<MavenPublication>("default") {
             from(components["java"])
+            artifact(sourcesJar.get())
         }
     }
 
